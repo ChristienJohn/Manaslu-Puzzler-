6d89febc8b6deeda50a3653a7c8cb147aaad88ac
ChristienJohn <ckjohn@sfu.ca>
2019-11-13 23:34:45 -0800

    Redid everything, grid and player movement done

--- Main.py
+++ Main.py
@@ -1,66 +1,176 @@
+import random as random
+import pygame as pygame
 
-import math
-import random
-import pygame
-import tkinter as tk
-from tkinter import messagebox
+pygame.init()                                 #start up dat pygame
+pygame.display.set_caption('Puzzler')
+clock = pygame.time.Clock()                   #for framerate or something? still not very sure
+Screen = pygame.display.set_mode([600, 600])  #making the window
+Done = False                                  #variable to keep track if window is open
+MapSize = 10                                  #how many tiles in either direction of grid
 
+TileWidth = 55                                #pixel sizes for grid squares
+TileHeight = 55
+TileMargin = 4.5
 
-
-class player(object):
-    def __init__(self, color, pos):
-        pass
-
-    def move(self):
-        pass
-
-    def reset(self, pos):
-        pass
-
-    def addCube(self):
-        pass
-
-    def draw(self, surface):
-        pass
+BLACK = (0, 0, 0)                             #some color definitions
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
 
 
-def drawGrid(w, rows, surface):
-    sizeBtwn = w // rows  # Gives us the distance between the lines
 
-    x = 0  # Keeps track of the current x
-    y = 0  # Keeps track of the current y
-    for l in range(rows):  # We will draw one vertical and one horizontal line each loop
-        x = x + sizeBtwn
-        y = y + sizeBtwn
-
-        pygame.draw.line(surface, (255, 255, 255), (x, 0), (x, w))
-        pygame.draw.line(surface, (255, 255, 255), (0, y), (w, y))
+class MapTile(object):                       #The main class for stationary things that inhabit the grid ... grass, trees, rocks and stuff.
+    def __init__(self, Name, Column, Row):
+        self.Name = Name
+        self.Column = Column
+        self.Row = Row
 
 
-def redrawWindow(surface):
-    global rows, width
-    surface.fill((0, 0, 0))  # Fills the screen with black
-    drawGrid(width, rows, surface)  # Will draw our grid lines
-    pygame.display.update()  # Updates the screen
+class Character(object):                    #Characters can move around and do cool stuff
+    def __init__(self, Name, HP, Column, Row):
+        self.Name = Name
+        self.HP = HP
+        self.Column = Column
+        self.Row = Row
+
+    def Move(self, Direction):              #This function is how a character moves around in a certain direction
+
+        if Direction == "UP":
+            if self.Row > 0:                #If within boundaries of grid
+                if self.CollisionCheck("UP") == False:       #And nothing in the way
+                   self.Row -= 1            #Go ahead and move
+
+        elif Direction == "LEFT":
+            if self.Column > 0:
+                if self.CollisionCheck("LEFT") == False:
+                    self.Column -= 1
+
+        elif Direction == "RIGHT":
+            if self.Column < MapSize-1:
+                if self.CollisionCheck("RIGHT") == False:
+                         self.Column += 1
+
+        elif Direction == "DOWN":
+            if self.Row < MapSize-1:
+                if self.CollisionCheck("DOWN") == False:
+                    self.Row += 1
+
+        Map.update()
+
+    def CollisionCheck(self, Direction):       #Checks if anything is on top of the grass in the direction that the character wants to move. Used in the move function
+        if Direction == "UP":
+            if len(Map.Grid[self.Column][(self.Row)-1]) > 1:
+                return True
+        elif Direction == "LEFT":
+            if len(Map.Grid[self.Column-1][(self.Row)]) > 1:
+                return True
+        elif Direction == "RIGHT":
+            if len(Map.Grid[self.Column+1][(self.Row)]) > 1:
+                return True
+        elif Direction == "DOWN":
+            if len(Map.Grid[self.Column][(self.Row)+1]) > 1:
+                return True
+        return False
+
+    def Location(self):
+        print("Coordinates: " + str(self.Column) + ", " + str(self.Row))
 
 
-def main():
-    global width, rows, s
-    width = 500  # Width of our screen
-    rows = 20  # Amount of rows
+class Map(object):              #The main class; where the action happens
+    global MapSize
 
-    surface = pygame.display.set_mode((width, width))  # Creates our screen object
+    Grid = []
 
-    s = player((254, 0, 0), (10, 10))
+    for Row in range(MapSize):     # Creating grid
+        Grid.append([])
+        for Column in range(MapSize):
+            Grid[Row].append([])
 
-    clock = pygame.time.Clock()  # creating a clock object
+    for Row in range(MapSize):     #Filling grid with grass
+        for Column in range(MapSize):
+            TempTile = MapTile("Grass", Column, Row)
+            Grid[Column][Row].append(TempTile)
 
-    flag = True
-    # STARTING MAIN LOOP
-    while flag:
-        pygame.time.delay(50)  # This will delay the game so it doesn't run too quickly
-        clock.tick(10)  # Will ensure our game runs at 10 FPS
-        redrawWindow(surface)  # This will refresh our screen
+    for Row in range(MapSize):     #Putting some rocks near the top
+        for Column in range(MapSize):
+            TempTile = MapTile("Rock", Column, Row)
+            if Row == 1:
+                Grid[Column][Row].append(TempTile)
+
+    for i in range(10):          #Placing Random trees
+        RandomRow = random.randint(0, MapSize - 1)
+        RandomColumn = random.randint(0, MapSize - 1)
+        TempTile = MapTile("Tree", RandomColumn, RandomRow)
+        Grid[RandomColumn][RandomRow].append(TempTile)
+
+    RandomRow = random.randint(0, MapSize - 1)      #Dropping the hero in
+    RandomColumn = random.randint(0, MapSize - 1)
+    Hero = Character("Hero", 10, RandomColumn, RandomRow)
+
+    def update(self):        #Very important function
+                             #This function goes through the entire grid
+                             #And checks to see if any object's internal coordinates
+                             #Disagree with its current position in the grid
+                             #If they do, it removes the objects and places it
+                             #on the grid according to its internal coordinates
+
+        for Column in range(MapSize):
+            for Row in range(MapSize):
+                for i in range(len(Map.Grid[Column][Row])):
+                    if Map.Grid[Column][Row][i].Column != Column:
+                        Map.Grid[Column][Row].remove(Map.Grid[Column][Row][i])
+                    elif Map.Grid[Column][Row][i].Name == "Hero":
+                        Map.Grid[Column][Row].remove(Map.Grid[Column][Row][i])
+        Map.Grid[int(Map.Hero.Column)][int(Map.Hero.Row)].append(Map.Hero)
+
+Map = Map()
+
+while not Done:     #Main pygame loop
+
+    for event in pygame.event.get():         #catching events
+        if event.type == pygame.QUIT:
+            Done = True
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            Pos = pygame.mouse.get_pos()
+            Column = Pos[0] // (TileWidth + TileMargin)  #Translating the position of the mouse into rows and columns
+            Row = Pos[1] // (TileHeight + TileMargin)
+            print(str(Row) + ", " + str(Column))
+
+            for i in range(len(Map.Grid[Column][Row])):
+                print(str(Map.Grid[Column][Row][i].Name))  #print stuff that inhabits that square
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                Map.Hero.Move("LEFT")
+            if event.key == pygame.K_RIGHT:
+                Map.Hero.Move("RIGHT")
+            if event.key == pygame.K_UP:
+                Map.Hero.Move("UP")
+            if event.key == pygame.K_DOWN:
+                Map.Hero.Move("DOWN")
+
+    Screen.fill(BLACK)
+
+    for Row in range(MapSize):           # Drawing grid
+        for Column in range(MapSize):
+            for i in range(0, len(Map.Grid[Column][Row])):
+                Color = WHITE
+                if len(Map.Grid[Column][Row]) == 2:
+                    Color = GREEN
+                if Map.Grid[Column][Row][i].Name == "Hero":
+                    Color = RED
 
 
-main()
+            pygame.draw.rect(Screen, Color, [(TileMargin + TileWidth) * Column + TileMargin,
+                                             (TileMargin + TileHeight) * Row + TileMargin,
+                                             TileWidth,
+                                             TileHeight])
+
+    clock.tick(60)      #Limit to 60 fps or something
+
+    pygame.display.flip()     #Honestly not sure what this does, but it breaks if I remove it
+    Map.update()
+
+pygame.quit()
